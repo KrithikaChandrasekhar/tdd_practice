13086108323cff59503405258f4f83121528b706
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-07-28 22:29:36 +0530

    .gitignore added

--- OddEven/oddeven_test.c
+++
@@ -1,7 +0,0 @@
-#include <assert.h>
-#include "oddeven.h"
-
-int main(void) {
-	assert(!iseven(0));
-	assert(iseven(2));
-}
--- PrimeNumber/primeNumber.c
+++
@@ -1,5 +0,0 @@
-#include "PrimeNumber.h"
-
-bool isPrimeNumber(int Number) {
-	return (Number == 2 || (Number % 2 == 1 && (Number % 4 == 1 || Number % 4 == 3) && Number > 2));
-}
