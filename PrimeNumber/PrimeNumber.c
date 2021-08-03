586661a793ad106c127fcfa3af770466545b99c4
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-08-03 11:26:10 +0530

    isPrimeNumber() simplified & true and false used as constant for bool values.

--- PrimeNumber/PrimeNumber.c
+++ PrimeNumber/PrimeNumber.c
@@ -2,13 +2,11 @@
 
 bool isPrimeNumber(int number) {
 	int i;
-	bool isPrime = 1;
+	bool isPrime = true;
 	for (i = 2; i <= number / 2; ++ i) {
-		if (number % i == 0) isPrime = 0;
+		if (number % i == 0) isPrime = false;
 	}
-	if (number == 0 || number == 1) return false;
-	else if (isPrime == 0) return false;
-	else return true;
+	return number > 1 && isPrime == true;
 }
 
 
