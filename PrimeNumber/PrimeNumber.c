3e7d305dd480a217d308397898ceda4c013869cb
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-08-09 12:03:42 +0530

    PrimeNumber.c

--- PrimeNumber/PrimeNumber.c
+++ PrimeNumber/PrimeNumber.c
@@ -1,13 +1,12 @@
 #include "PrimeNumber.h"
 
-bool
-isPrimeNumber (unsigned int number)
+bool isPrimeNumber(unsigned int number)
 {
- unsigned int i;
-  if (number <= 1)
-    return false;
-  for (i = 2; i <= number / 2; ++i)
-    if (number % i == 0)
-      return false;
-  return true;
+    unsigned int i;
+    if (number <= 1)
+	return false;
+    for (i = 2; i <= number / 2; ++i)
+	if (number % i == 0)
+	    return false;
+    return true;
 }
