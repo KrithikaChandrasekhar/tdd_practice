df69d3c6e08847a89bee578f1cf2345ac9e92523
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-08-15 16:29:58 +0530

    echo.c

--- Echo/echo.c
+++ Echo/echo.c
@@ -3,9 +3,11 @@
 int main(int argc, char* argv[]) {
    int i;
    if (argc > 1) {
-       for (i = 1; i < argc; i++)
-       puts(argv[i]);
+       for (i = 1; i < argc; i++) {
+            if(argv[i] != NULL)
+	    printf("%s\n", argv[i]);
+            //puts(argv[i]);
        }
-    else
-    puts(" ");
+   }
+    else puts(" ");
 }
