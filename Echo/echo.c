46cc4525f909af2a26bca25310956385c6d4a223
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-08-17 16:54:44 +0530

    echo

--- Echo/echo.c
+++ Echo/echo.c
@@ -2,16 +2,16 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-   int i;
-   char str1[]= "Implementing echo";
-   char str2[] = " in c ";
-   
-   if (argc > 1) {
+    int i;
+    char str1[]= "Implementing echo";
+    char str2[] = " in c ";
+
+    if (argc > 1) {
        for (i = 1; i < argc; i++) {
             if(argv[i] != NULL)
-	    printf("%li\n",strlen(argv[i]));
+               printf("%li\n",strlen(argv[i]));
        }
    }
    else printf("\n");
-//   printf("%s\n",strcat(str1, str2));
+//   printf("%s\n",strncat(str1, str2, 3));
 }
--- Echo/echo1.c
+++ Echo/echo1.c
@@ -3,20 +3,15 @@
 #include <stdlib.h>
 
 int main (int argc, int *argv[]) {
-    if(argc==1) {
-       printf("You have not entered anything!\n");
-       return 0;
-    }
-    char *arr=malloc(1000*sizeof(char));
-    int i;
-    strcat(arr, argv[0]);
-    strcat(arr, " ");
-    for (i = 0; i < argc - 1; i++) {
-         strcat(arr,argv[i+1]);
-	 strcat(arr, " ");
-    }
 
-    printf("%s\n",arr);
-    free(arr);
-    return 0;
+    if (argc > 1) {
+       int i, n;
+       char *arr = malloc(n * sizeof(char));
+       for (i = 1; i < argc; i++) {
+           strncat(arr, argv[i], 2);
+           printf("%s", arr);
+       }
+       free(arr);
+    }
+    else printf("\n");
 }
