c71056e6a825731e68a60dbf48f731236244a2d2
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-08-19 15:05:27 +0530

    Unix program 'head' implemented with buffer & without buffer in C

--- head/head.c
+++ head/head.c
@@ -1,16 +1,20 @@
-#include<stdio.h>
-
-#define DATA_SIZE 1024
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
     FILE *file;
-    int line_number = 1;
-    int input_char;
-    
-    file = fopen(argv[1], "r");
-    while (fgetc(file) != NULL && line_number != 10) {
-          fprintf (file, "%c", input_char);
-	  line_number++;
+    char *line = NULL;
+    size_t len = 0;
+    int n = 10;
+
+    if (file = fopen(argv[1], "r")) {
+        while (getline(&line, &len, file) != -1) {
+	       if (n == 0) break;
+	       else printf("%s", line);
+	       n--;
+	}
+        fclose(file);
     }
+    else fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[1]);
 }
 
