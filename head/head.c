9fbd71cb8d85d803ca5e51665260bd2024411615
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-08-19 15:47:30 +0530

    Unix program 'head' implementation with buffer & without buffer in C

--- head/head.c
+++ head/head.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
-    int n = 10;
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	int n = 10;
 
-    if (file = fopen(argv[1], "r")) {
-        while (getline(&line, &len, file) != -1) {
-	       if (n == 0) break;
-	       else printf("%s", line);
-	       n--;
+	if (file = fopen(argv[1], "r")) {
+		while (getline(&line, &len, file) != -1) {
+			if (n == 0) break;
+			else printf("%s", line);
+			n--;
+		}
+		fclose(file);
 	}
-        fclose(file);
-    }
-    else fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[1]);
+	else fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[1]);
 }
 
--- head/head_buffer.c
+++ head/head_buffer.c
@@ -4,26 +4,26 @@
 #define DATA_SIZE 1024
 
 int main (int argc, char* argv[]) {
-    FILE *file;
-    char data[DATA_SIZE];
-    int n = 10;
+	FILE *file;
+	char data[DATA_SIZE];
+	int n = 10;
 
-    while (argc != 2) {
-           if (gets(data, stdin) != NULL) {
-	       puts(data);
-	   }
-	   else break;
-    }
-    if (file = fopen(argv[1],"r")) {
-        while (fgets(data, sizeof(data),file)) {
-              if (n == 0) break;
-              printf("%s", data);
-              n--;
-        }
-        fclose(file);
-    }
-    else fprintf (stderr, "%s: %s: No such file or directory\n", argv[0], argv[1]);
+	while (argc == 1) {
+		if (fgets(data, sizeof(data), stdin) != NULL) {
+			fputs(data, stdout);
+		}
+		else break;
+	}
+	for(int i = 1; i < argc; i++) {
+		if (file = fopen(argv[i], "r")) {
+			while (fgets(data, sizeof(data), file)) {
+				if (n == 0) break;
+				printf("%s", data);
+				n--;
+			}
+			fclose(file);
+		}
+
+		else fprintf (stderr, "%s: %s: No such file or directory\n", argv[0], argv[1]);
+	}
 }
-
-
-
