2380291b1fbceee4f859fdfa8f25833ca184143b
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-08-21 13:43:59 +0530

    head.c loop over the arguments like cat

--- head/head.c
+++ head/head.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
 	FILE *file;
 	char *line = NULL;
 	size_t len = 0;
-	int n = 10;
+	int linenum = 10;
+	int inputchar;
 
-	if (file = fopen(argv[1], "r")) {
-		while (getline(&line, &len, file) != -1 && n != 0) {
-			printf("%s", line);
-			n--;
-		}
-		fclose(file);
+	if (argc == 1) {
+		while ((inputchar = fgetc(stdin)) != EOF)
+			fputc(inputchar, stdout);
 	}
-	else fprintf(stderr, "%s: %s: No such file or directory\n", argv[0], argv[1]);
+	else
+		for(int i = 1; i < argc; i++) {
+			if ((file = fopen(argv[i], "r")) != NULL) {
+				while (getline(&line, &len, file) != -1 && linenum != 0) {
+					fputs(line, stdout);
+					linenum--;
+				}
+				fclose(file);
+				linenum = 10;
+			}
+			else fprintf(stderr, "%s: %s: %s\n", argv[0], argv[i], strerror(errno));
+		}
 }
 
