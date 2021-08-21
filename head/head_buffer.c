44a3e51872bf06813a95345819d236ed2d0b6684
Krithika Chandrasekhar <krithikac1994@gmail.com>
2021-08-21 17:00:54 +0530

    unix 'head' program implemented in C

--- head/head_buffer.c
+++ head/head_buffer.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 #define DATA_SIZE 1024
 
 int main (int argc, char* argv[]) {
 	FILE *file;
 	char data[DATA_SIZE];
-	int n = 10;
+	int linenum = 10;
+	int stringlen;
 
-	while (argc == 1) {
-		if (fgets(data, sizeof(data), stdin) != NULL) {
+	if (argc == 1) {
+		while (fgets(data, sizeof(data), stdin) != NULL)
 			fputs(data, stdout);
-		}
-		else break;
 	}
-	for(int i = 1; i < argc; i++) {
-		if (file = fopen(argv[i], "r")) {
-			while (fgets(data, sizeof(data), file)) {
-				if (n == 0) break;
-				printf("%s", data);
-				n--;
+	else
+		for(int i = 1; i < argc; i++) {
+			if (file = fopen(argv[i], "r")) {
+				if (argc > 2) printf("==> %s <==\n", argv[i]);
+				while ((fgets(data, sizeof(data), file) != NULL) && linenum != 0) {
+					printf("%s", data);
+					linenum--;
+				}
+				fclose(file);
+				if ((i < argc - 1) && (file = fopen(argv[i+1], "r"))) printf("\n");
+				linenum = 10;
 			}
-			fclose(file);
+			else fprintf (stderr, "%s: cannot open '%s' for reading: %s\n", argv[0], argv[i], strerror(errno));
 		}
-
-		else fprintf (stderr, "%s: %s: No such file or directory\n", argv[0], argv[1]);
-	}
 }
